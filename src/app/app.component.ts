6155f5fa340dcce8df0cc8d46bc8abfd64711cba
Casey Kawamura <35353414+Yellowcasey@users.noreply.github.com>
2019-04-10 20:47:10 -0600

    2 more pages
    Added sidemenu option 'tools' and made a basic layout for the activity one page with a button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'Lessons',
       url: '/list',
       icon: 'list'
+    },
+    {
+     title: 'Tools',
+     url: '/tools',
+     icon: 'build'
     }
   ];
 
