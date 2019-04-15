000b4f7f480600c9246f5fbc38516af33e7f48c8
Casey Kawamura <35353414+Yellowcasey@users.noreply.github.com>
2019-04-15 15:35:15 -0600

    ignored api-keys and installed rxjs-compat
    Couldn't find rxjs-compat/* modules.
    
    There was no rxjs-compat folder, so I tried installing it and it fixed the problem.
    
    npm install --save rxjs-compat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { masterFirebaseConfig } from './api-keys';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -8,6 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+};
 
 @NgModule({
   declarations: [AppComponent],
