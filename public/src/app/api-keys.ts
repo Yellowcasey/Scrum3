8db19725a75108c9a9405023dffb89857cd92632
DJ Roychoudhury <debjitroychoudhury@u.boisestate.edu>
2019-04-25 01:16:34 -0600

    fixed issues with angular 2 -firebase compatibility. needed updated version with -rc.7 suffix

--- public/src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-    apiKey: "AIzaSyBwUAPATZztGPrsGkphPrVA1_44ntP-1rs",
-    authDomain: "philtest-249a9.firebaseapp.com",
-    databaseURL: "https://philtest-249a9.firebaseio.com",
-    projectId: "philtest-249a9",
-    storageBucket: "philtest-249a9.appspot.com",
-    messagingSenderId: "615387791494"
-};
