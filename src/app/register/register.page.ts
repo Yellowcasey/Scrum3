93350953822c710d72b0a230bff60cb848c180ec
Casey Kawamura <35353414+Yellowcasey@users.noreply.github.com>
2019-04-30 22:38:54 -0600

    registration page working
    Working registration page for users to sign up

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase/app'
+import { AngularFireAuth } from 'angularfire2/auth'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  username: string = ""
+  password: string = ""
+  cpassword: string = ""
+
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
+  async register() {
+
+    const {username, password, cpassword} = this
+    if(password !== cpassword) {
+      return console.error("Your passwords do not match")
+    }
+
+    try {
+      const res = await this.afAuth.auth.createUserWithEmailAndPassword(username, password)
+      console.log("A user has been created");
+        }catch(error){
+          console.dir(error)
+        }
+}
 }
