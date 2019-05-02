0add5137f079ee40db7a595b229f2442ce83da69
DJ Roychoudhury <debjitroychoudhury@u.boisestate.edu>
2019-05-02 09:10:14 -0700

    fixed merge conflict

--- src/app/vocabulary.service.ts
+++ src/app/vocabulary.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-=======
->>>>>>> master
-
 @Injectable({
   providedIn: 'root'
 })
